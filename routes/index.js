429fd6e37bb212bae532b5ad8616ccd6707aadaa
Charlie Appleton <xjarlie@gmail.com>
2022-09-08 16:22:55 +0900

    add template routing

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,16 @@ router.get('/', (req, res) => {
     res.status(200).render('index', { data: data });
 });
 
+router.get('/motif/:motifID', (req, res) => {
+    let motif = {};
+
+    res.status(200).render('motif', { motif: motif });
+});
+
+router.get('/track/:trackID', (req, res) => {
+    let track = {};
+
+    res.status(200).render('track', { track: track });
+});
+
 module.exports = router;
