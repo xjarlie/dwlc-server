da9e139cf1c14a03ba5aa16a918cf63e8d889941
Charlie Appleton <xjarlie@gmail.com>
2022-09-08 19:58:04 +0900

    wasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["views/index.js"],
+  content: ["views/*.ejs", "views/partials/*.ejs"],
   theme: {
     extend: {},
   },
