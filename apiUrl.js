af824477339dfeda28b4c5fa71829c61e0aee267
Charlie Appleton <xjarlie@gmail.com>
2022-09-09 00:16:56 +0900

    add motif fetching

---
+++ apiUrl.js
@@ -0,0 +1,3 @@
+const apiUrl = 'https://dwlc-api.xjarlie.com/api/graphql';
+
+module.exports = apiUrl;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const apiUrl = require('../apiUrl');
 const router = express.Router();
+const fetch = require('node-fetch');
 
 router.get('/', (req, res) => {
 
@@ -13,10 +15,41 @@ router.get('/', (req, res) => {
     res.status(200).render('index', { data: data });
 });
 
-router.get('/motif/:motifID', (req, res) => {
-    let motif = {};
+router.get('/motif/:motifID', async (req, res) => {
+    const { motifID } = req.params;
+    const query = `
+        {
+            motif(where: { id: "${motifID}"}) {
+                id
+                name
+                represents
+                notes
+                tags {
+                    name
+                }
+                appearances {
+                    id
+                    name
+                    url
+                    appears
+                    notes
+                    main
+                }
+            }
+        }
+    `
 
-    res.status(200).render('motif', { motif: motif });
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query })
+    });
+    const status = response.status;
+    const motif = (await response.json()).data.motif;
+
+    res.status(200).render('motif.ejs', { motif: motif });
 });
 
 router.get('/track/:trackID', (req, res) => {
